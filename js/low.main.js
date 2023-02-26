4ab5279a3c37442b883aa788d82f9c2a58912e94
Balmiki kumar <88521022+Balmikiii@users.noreply.github.com>
2023-02-26 14:19:24 +0530

    Add files via upload

---
+++ js/low.main.js
@@ -0,0 +1,402 @@
+var myQuestions = [
+    {
+      question: "a: 'What is the programming language to learn in 2020What is 10/2?",
+      answers: {
+        a: 'HTML',
+        b: 'Python',
+        c: 'Javascript',
+        d: 'Swift115'
+        },
+      correctAnswer: 'b'
+    },
+    {
+      question: "b: 'The full form in CSS is:",
+      answers: {
+        a: 'Cascading style sheet3',
+        b: 'Coloured Special Sheets',
+        c: 'Color and Style Sheets',
+        d: 'D. None of the above',
+      },
+      correctAnswer: 'a'
+    },
+    {
+      question: "c: 'How can we chenge the background color of an element?",
+      answers: {
+        a: 'A. Background',
+        b: 'Color',
+        c: 'Both A and B',
+        d: 'None of the above',
+      },
+      correctAnswer: 'a'
+    },
+    {
+      question: "d: 'In how many ways can CSS be written in?",
+      answers: {
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '4',
+      },
+      correctAnswer: 'c'
+    },
+    {
+      question: "5.Which HTML tag is used to declare internal CSS?",
+      answers: {
+        a: 'Style',
+        b: 'Link',
+        c: 'Script',
+        d: 'None of the above',
+      },
+      correctAnswer: 'a'
+    },
+    {
+      question: "6.How to create an ordered list in HTML",
+      answers: {
+        a: 'ul',
+        b: 'ol',
+        c: 'Shref',
+        d: 'b',
+      },
+      correctAnswer: 'a'
+    },
+
+    // balmik---------------------------------------------------------------------------------
+    {
+      question: "What type of CSS is generally recommended for designing large web pages?",
+      answers: {
+        a: 'inline',
+        b: 'internal',
+        c: 'internal',
+        d: 'none of the above',
+      },
+      correctAnswer: 'c'
+    },
+    {
+      question: "In the below code snippet, in what order will the margins be added?",
+      answers: {
+        a: 'top,right,bottom,left',
+        b: 'top,left,bottom,right',
+        c: 'top,bottom,right,left',
+        d: 'right,left,top,bottom',
+      },
+      correctAnswer: 'a'
+    },
+    {
+      question: "Can negative values be allowed in padding property?",
+      answers: {
+        a: 'yes',
+        b: 'no',
+        c: 'depeds on property',
+        d: 'none of the above',
+      },
+      correctAnswer: 'b'
+    },
+    {
+      question: "The CSS property used to specify the transparency of an element is?",
+      answers: {
+        a: 'opacity',
+        b: 'visibility',
+        c: 'filter',
+        d: 'none of the above',
+      },
+      correctAnswer: 'a'
+    },
+    {
+        question: "How can we specify the spacing between each letter in a text in CSS?",
+        answers: {
+            a: 'alpha-spacing',
+            b: 'letter-spacing',
+            c: 'character-spacing',
+            d: 'none of the above',
+    },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Which of the following are valid ways to represent a colour in CSS",
+        answers: {
+            a: 'valid color name',
+            b: 'RGB values',
+            c: 'HEX values',
+            d: 'all of the above',
+    },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Which of the following is the correct way to select all h1 headers in a div element?",
+        answers: {
+            a: 'div h1',
+            b: ' div-h1',
+            c: 'h1',
+            d: 'none of the above',
+    },
+        correctAnswer: 'a'
+    },
+    {
+    question: "We can make rounded borders around elements using which CSS element?",
+    answers: {
+        a: 'border-collapse',
+        b: 'border-round',
+        c: 'border-radius',
+        d: 'none of the above',
+    },
+        correctAnswer: 'c'
+    },
+    {
+    question: "Which of the following property specifies the look and design of an outline?",
+    answers: {
+        a: 'outline-style',
+        b: 'outline',
+        c: 'outline-font',
+        d: 'none of the above',
+    },
+        correctAnswer: 'a'
+    },
+    {
+ 
+    question: "Which of the following are valid CSS position property values?",
+    answers: {
+        a: 'static',
+        b: 'relative',
+        c: 'fixed',
+        d: 'all of the above',
+    },
+        correctAnswer: 'd'
+    },
+    {
+    question: "What does the inherit value for a property do?",
+    answers: {
+        a: ' inherits the value of the parent element',
+        b: 'invalid property name ',
+        c: 'chenges the value of the elements property',
+        d: 'none of the above',
+    },
+        correctAnswer: 'a'
+    },
+    {
+ 
+    question: "Setting an inline-block in CSS requires which of the following properties?",
+    answers: {
+        a: 'display',
+        b: 'color',
+        c: 'block',
+        d: 'none of the above',
+    },
+        correctAnswer: 'a'
+    },
+    {
+    question: "Which of the following CSS property is used for controlling the layout?",
+    answers: {
+        a: 'display',
+        b: 'color',
+        c: 'text',
+        d: 'none of the above',
+    },
+        correctAnswer: 'a'
+    },
+    {
+    question: "Which of the following CSS property specifies the origin of the background image?",
+    answers: {
+        a: 'background-origin',
+        b: 'background-size',
+        c: 'origin',
+        d: 'none of the above',
+    },
+        correctAnswer: 'a'
+    },
+    {
+ 
+    question: "Which of the following are parts of the CSS box model?",
+    answers: {
+        a: 'margins',
+        b: 'borders',
+        c: 'padding',
+        d: 'all of the above',
+    },
+        correctAnswer: 'd'
+    },
+    {
+ 
+    question: "Which of the following components of the CSS box model are transparent?",
+    answers: {
+        a: 'padding',
+        b: 'margin',
+        c: 'both A and B',
+        d: 'content',
+    },
+        correctAnswer: 'c'
+    },
+    {
+    question: "Which of the following can be achieved with CSS lists?",
+    answers: {
+        a: 'set different list item markers for ordered list',
+        b: 'set different list item markers for unordered lists',
+        c: 'set an image as the list item marker',
+        d: 'all of the above',
+    },
+        correctAnswer: 'd'
+    },
+    {
+
+    question: "Which of the following CSS property specifies the type of list item marker?",
+    answers: {
+        a: ' list',
+        b: 'lis-style-type',
+        c: 'ul',
+        d: 'ol',
+    },
+        correctAnswer: 'b'
+    },
+    {
+    question: "What value should be passed to the width parameter if we want an element, say a table, to span the entire width of the screen?",
+    answers: {
+        a: '100%',
+        b: '50%',
+        c: '0%',
+        d: 'none of the above',
+    },
+        correctAnswer: 'a'
+    },
+    {
+ 
+    question: "Which of the following properties is used to align text in CSS?",
+    answers: {
+        a: 'text-align',
+        b: 'text-alignment',
+        c: 'text',
+        d: 'text-position',
+    },
+        correctAnswer: 'a'
+    },
+    {
+    question: "Which of the following is the correct approach to make a table responsive?",
+    answers: {
+        a: 'overflow-x:auto',
+        b: 'overflow-x:none',
+        c: 'both A and B',
+        d: 'none of the above',
+    },
+        correctAnswer: 'a'
+    },
+    {
+    question: "Which of the following CSS property creates a clipping region and specifies the visible area of the element?",
+    answers: {
+        a: 'visibility',
+        b: 'cip-path',
+        c: 'both A and B',
+        d: 'none of the above',
+    },
+        correctAnswer: 'b'
+    },
+    {
+    question: "Which of the following are types of length units in CSS?",
+    answers: {
+        a: 'absolute',
+        b: 'relative',
+        c: 'both A and B',
+        d: 'none of the above',
+    },
+        correctAnswer: 'c'
+    },
+    {
+    question: "Which of the following are units of relative length in CSS?",
+    answers: {
+        a: 'em',
+        b: 'rem',
+        c: 'vmax',
+        d: 'all of the above',
+    },
+        correctAnswer: 'd'
+    },
+
+
+
+    
+  ];
+  
+  var quizContainer = document.getElementById('quiz');
+  var resultsContainer = document.getElementById('results');
+  var submitButton = document.getElementById('submit');
+  
+  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+  
+  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+  
+    function showQuestions(questions, quizContainer){
+      // we'll need a place to store the output and the answer choices
+      var output = [];
+      var answers;
+  
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+        
+        // first reset the list of answers
+        answers = [];
+  
+        // for each available answer...
+        for(letter in questions[i].answers){
+  
+          // ...add an html radio button
+          answers.push(
+            '<label>'
+              + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+              + letter + ': '
+              + questions[i].answers[letter]
+            + '</label>'
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+          '<div class="question">' + questions[i].question + '</div>'
+          + '<div class="answers">' + answers.join('') + '</div>'
+        );
+      }
+  
+      // finally combine our output list into one string of html and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+  
+    function showResults(questions, quizContainer, resultsContainer){
+      
+      // gather answer containers from our quiz
+      var answerContainers = quizContainer.querySelectorAll('.answers');
+      
+      // keep track of user's answers
+      var userAnswer = '';
+      var numCorrect = 0;
+      
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+  
+        // find selected answer
+        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+        
+        // if answer is correct
+        if(userAnswer===questions[i].correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+          
+          // color the answers green
+          answerContainers[i].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[i].style.color = 'red';
+        }
+      }
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+  
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+      showResults(questions, quizContainer, resultsContainer);
+    }
+  
+  }
